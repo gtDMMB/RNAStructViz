b9eada26cbeba4ae99a455e76e1e1fdf0e9b90c8
maxieds <maxieds@gmail.com>
2018-11-16 12:06:21 -0500

    More detailed error handler

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -285,7 +285,7 @@ void MainWindow::ConfigOptionsCallback(Fl_Widget* widget, void* userData) {
      cfgWindow->show();
      while(!cfgWindow->isDone() && cfgWindow->visible()) 
           Fl::wait();
-     RethemeMainWindow();
+     //RethemeMainWindow();
      delete cfgWindow;
 
 }
