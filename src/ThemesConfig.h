ade7df0ac1bdb186d8613231cf98fd6b54f753bd
maxieds <maxieds@gmail.com>
2018-11-16 02:56:27 -0500

    More preset built-in color themes defined

--- src/ThemesConfig.h
+++ src/ThemesConfig.h
@@ -45,7 +45,7 @@ static const ColorTheme_t PRESET_COLOR_THEMES[] = {
 	  Fl::get_color(58), 
 	  Fl::get_color(18), 
 	  Fl::get_color(132), 
-	  "Green On Orange",
+	  "Green On Orange", // a good color combination
 	  true
      },
      {
@@ -54,9 +54,36 @@ static const ColorTheme_t PRESET_COLOR_THEMES[] = {
 	  0x00da0000, 
 	  0xd3d3b200,
           0x16161600, 
-	  "Green On Black", 
+	  "Green On Black", // standard Linux / command line colors
 	  true
      }, 
+     {
+          0xd5008600,
+	  0xad33c000,
+	  0x23222400,
+	  0x7c052700,
+	  Darker(0xd5008600, 0.7f), 
+	  "Chicky", // very pink, also one of my favorite Android phone themes
+	  true
+     },
+     {
+          0xeeb50000,
+	  0xff6d0000,
+	  0x3e3c4000,
+	  0x604f0900,
+	  Darker(0xeeb50000, 0.7f), 
+	  "Sunshine", // very vibrant and bright
+	  true
+     },
+     {
+          0x98a8a800,
+	  0x3a6ea500,
+	  0xdffff300,
+	  0x24241f00,
+	  Darker(0x98a8a800, 0.65f), 
+	  "Redmond", // old school Windows 2000 era color scheme
+	  true
+     },
      {
           0, 
 	  0, 
