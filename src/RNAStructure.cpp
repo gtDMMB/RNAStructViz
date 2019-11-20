e57385e11c818a64dc291fe8f435fb384378b742
Maxie D. Schmidt <maxieds@gmail.com>
2019-11-20 07:01:39 -0500

    Mac requires an initial redraw() of the CT viewer window

--- src/RNAStructure.cpp
+++ src/RNAStructure.cpp
@@ -1089,6 +1089,7 @@ void RNAStructure::DisplayFileContents(const char *titleSuffix)
     m_contentWindow->callback(CloseCTViewerContentWindowCallback);
     m_contentWindow->show();
     m_contentWindow->make_current();
+    m_contentWindow->redraw();
 
 }
 
