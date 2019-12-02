cd75e60576e168602d3ea07d5e912f05310aa13b
Maxie D. Schmidt <maxieds@gmail.com>
2019-12-02 00:32:16 -0500

    Fixing small scripting error in build scripts

--- build-scripts/get-build-config-setting.sh
+++ build-scripts/get-build-config-setting.sh
@@ -3,7 +3,7 @@
 #### get-build-config-setting.sh : Grok a build config setting out of the specified BuildConfig.cfg files;
 #### Usage: ./get-build-config-setting.sh <BUILD-CONFIG-CFGFILE-PATH> <CONFIG-SETTING-STRING-SPEC>
 
-if [[ $# -lt 2]]; then
+if [[ $# -lt 2 ]]; then
 	echo "Invalid parameters passed to script $0 ... Exiting! (-1)"
 	exit -1;
 fi
